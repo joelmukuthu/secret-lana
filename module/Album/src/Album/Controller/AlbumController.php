b46efe00320081b91ca8097e3aeac19952c889c7
Joel Mukuthu <joel.mukuthu@citat.fi>
2014-07-02 17:15:22 +0300

    album module initial commit

---
+++ module/Album/src/Album/Controller/AlbumController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Album\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+class AlbumController extends AbstractActionController
+{
+  public function indexAction() {
+  }
+  public function addAction() {
+  }
+  public function editAction() {
+  }
+  public function deleteAction() {
+  }
+}
